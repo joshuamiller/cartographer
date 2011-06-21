eb85ff06fe6eb71230478d5331436ad4ea725bf8
Gyorgy Frivolt <fifigyuri@gmail.com>
2011-06-21 10:27:12 +0200

    support for label parameter used by MarkerWithLabel utility class

--- lib/v3/cartographer/gmarker.rb
+++ lib/v3/cartographer/gmarker.rb
@@ -1,6 +1,6 @@
 class Cartographer::Gmarker
   #include Reloadable
-  attr_accessor :name, :marker_type, :highlight, :icon, :position, :click, :info_window, :info_window_url, :map, :min_zoom, :max_zoom, :dblclick, :draggable
+  attr_accessor :name, :marker_type, :highlight, :icon, :position, :click, :info_window, :info_window_url, :map, :min_zoom, :max_zoom, :dblclick, :draggable, :label
 
   def initialize(options = {})
     @name = options[:name] || "marker"
@@ -14,6 +14,7 @@ class Cartographer::Gmarker
     @map = options[:map]
     @highlight = options[:highlight] || false
     @draggable = options[:draggable] || false
+    @label = options[:label] || nil
   
     # inherit our 'debug' settings from the map, if there is one, and it's in debug
     # you can also just debug this marker, if you like, or debug the map and
@@ -55,7 +56,19 @@ class Cartographer::Gmarker
     marker_clusterer = marker_clusterer_flag
     script = []
     script << "// Set up the pre-defined marker" if @debug
-    script << "#{@name} = new google.maps.Marker({map: null, position: new google.maps.LatLng(#{@position[0]}, #{@position[1]}), draggable: #{@draggable}, icon: #{@icon.name}, shadow: #{@icon.name}_shadow}); \n"
+    if @label
+      label_options = []
+      @label.each do |key, value|
+        if :anchor == key
+          label_options << "label#{key.to_s.capitalize}:#{value.to_s}"
+        else
+          label_options << "label#{key.to_s.capitalize}:'#{value.to_s}'"
+        end
+      end
+      script << "#{@name} = new MarkerWithLabel({map: null, position: new google.maps.LatLng(#{@position[0]}, #{@position[1]}), draggable: #{@draggable}, icon: #{@icon.name}, shadow: #{@icon.name}_shadow,#{label_options.join(',')}});\n"
+    else
+      script << "#{@name} = new google.maps.Marker({map: null, position: new google.maps.LatLng(#{@position[0]}, #{@position[1]}), draggable: #{@draggable}, icon: #{@icon.name}, shadow: #{@icon.name}_shadow}); \n"
+    end
 
     if @click
       script << "// Create the listener for your custom click event" if @debug
